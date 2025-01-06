660a148d253f289dcc58c66f7cf3d85fd4610136
erik <erlo22@student.bth.se>
2025-01-06 21:02:31 +0100

    Added polygon circle, consts and some references to mapbox

--- app/src/main/java/com/team13/karlskronaexplorer/view/FindView.kt
+++ app/src/main/java/com/team13/karlskronaexplorer/view/FindView.kt
@@ -1,8 +1,12 @@
+// https://docs.mapbox.com/android/maps/guides/user-location/location-on-map/
+// https://docs.mapbox.com/android/navigation/guides/device-location/
+// https://docs.mapbox.com/android/maps/guides/annotations/annotations/#polygon-annotation
+
+
 package com.team13.karlskronaexplorer.view
 
 import android.animation.ValueAnimator
 import android.util.Log
-import android.view.Window
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -16,16 +20,14 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -42,18 +44,23 @@ import com.mapbox.geojson.Point
 import com.mapbox.maps.extension.compose.MapEffect
 import com.mapbox.maps.extension.compose.MapboxMap
 import com.mapbox.maps.extension.compose.animation.viewport.rememberMapViewportState
-import com.mapbox.maps.extension.compose.annotation.generated.withCircleColor
-import com.mapbox.maps.extension.style.expressions.dsl.generated.pitch
+import com.mapbox.maps.extension.compose.annotation.generated.PolygonAnnotation
 import com.mapbox.maps.plugin.PuckBearing
-import com.mapbox.maps.plugin.annotation.annotations
-import com.mapbox.maps.plugin.annotation.generated.CircleAnnotationOptions
-import com.mapbox.maps.plugin.annotation.generated.createCircleAnnotationManager
 import com.mapbox.maps.plugin.locationcomponent.LocationConsumer
 import com.mapbox.maps.plugin.locationcomponent.createDefault2DPuck
 import com.mapbox.maps.plugin.locationcomponent.location
 import com.team13.karlskronaexplorer.domain.Position
 import com.team13.karlskronaexplorer.domain.Post
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
 import kotlin.random.Random
+
+const val DIST_SUCCESS = 10
+const val DIST_CLOSER = 100
+
+const val CIRCLE_COLOR_RED = 0xffee4e8b
+const val CIRCLE_COLOR_GREEN = 0xff00ee00
 
 class locConsumer(private val callback: (Position) -> Unit) : LocationConsumer {
 	override fun onBearingUpdated(vararg bearing: Double, options: (ValueAnimator.() -> Unit)?) {
@@ -86,7 +93,7 @@ class locConsumer(private val callback: (Position) -> Unit) : LocationConsumer {
 @Composable
 fun FindView(innerPadding: PaddingValues, activePost: Post, unselect: ()->Unit) {
 	val mapViewportState = rememberMapViewportState()
-	var circleColor by remember { mutableStateOf(Color.Red) }
+	var circleColor by remember { mutableLongStateOf(CIRCLE_COLOR_RED) }
 	var locationFound by remember { mutableStateOf(false)}
 	val rand = Random(seed = activePost.getRandomSeed())
 	val randomLoc1 = rand.nextDouble(0.0001,0.0003)
@@ -124,7 +131,7 @@ fun FindView(innerPadding: PaddingValues, activePost: Post, unselect: ()->Unit) 
 		Modifier.fillMaxSize().padding(innerPadding),
 		mapViewportState = mapViewportState,
 	) {
-		MapEffect(circleColor) { mapView ->
+		MapEffect(Unit) { mapView ->
 			mapView.location.updateSettings {
 				locationPuck = createDefault2DPuck(withBearing = true)
 				enabled = true
@@ -132,40 +139,32 @@ fun FindView(innerPadding: PaddingValues, activePost: Post, unselect: ()->Unit) 
 				puckBearingEnabled = true
 			}
 			mapViewportState.transitionToFollowPuckState()
-			val annotationApi = mapView.annotations
-			val circleAnnotationManager = annotationApi.createCircleAnnotationManager()
-			val circleAnnotationOptions: CircleAnnotationOptions = CircleAnnotationOptions()
-				.withPoint(
-					Point.fromLngLat(
-						activePost.getPosition().getLongitude() + randomLoc1,
-						activePost.getPosition().getLatitude() + randomLoc2
-					)
-				)
-
-				.withCircleRadius(150.0)
-				.withCircleOpacity(0.3)
-				.withCircleColor(circleColor)
-				.withCircleStrokeWidth(2.0)
-				.withDraggable(false)
-
-			circleAnnotationManager.deleteAll()
-			circleAnnotationManager.create(circleAnnotationOptions)
 
 			mapView.location.getLocationProvider()?.registerLocationConsumer(locConsumer({position ->
-					var dist = position.distanceTo(activePost.getPosition())
-					if (dist < 10) {
-						locationFound = true
-					}
-					else if (dist < 150) {
-						circleColor = Color.Green
-					} else {
-						circleColor = Color.Red
-					}
-					Log.d("test", dist.toString())
+				val dist = position.distanceTo(activePost.getPosition()).toInt()
+				if (dist < DIST_SUCCESS) {
+					locationFound = true
+				}
+				circleColor = if (dist < DIST_CLOSER) {
+					CIRCLE_COLOR_GREEN
+				} else {
+					CIRCLE_COLOR_RED
+				}
+				Log.d("test", dist.toString())
 			}))
+		}
+			PolygonAnnotation(
+				points = listOf(
+					getListOfCirclePoints(
+						Position(activePost.getPosition().getLatitude()+randomLoc1,
+								activePost.getPosition().getLongitude()+randomLoc2))
+				)
+			) {
+				fillColor = Color(circleColor)
+				fillOpacity = 0.4
+			}
 
 		}
-	}
 	Column(Modifier.fillMaxWidth().padding(0.dp,15.dp), horizontalAlignment = Alignment.CenterHorizontally) {
 		Image(
 			activePost.getImage().asImageBitmap(),
@@ -181,3 +180,13 @@ fun FindView(innerPadding: PaddingValues, activePost: Post, unselect: ()->Unit) 
 	}
 }
 
+fun getListOfCirclePoints(position: Position): MutableList<Point> {
+	val circlePoints = mutableListOf<Point>()
+	for (i in 0..360) {
+		val r = i.toDouble() / 180 * PI
+		circlePoints.add(
+			Point.fromLngLat(position.getLongitude() + cos(r)/250,
+				position.getLatitude()+ sin(r)/500))
+	}
+	return circlePoints
+}
